fbfcbf4bd8e7553b706b7dfbacfa476bd598fc8a
Alistair Campbell <65419433+amcampbell1975@users.noreply.github.com>
2022-09-04 15:49:50 +0100

    Update roost.py
    Add logging and delta temperature

--- roost.py
+++ roost.py
@@ -4,6 +4,8 @@ import time
 import json
 import logging
 
+VERSION="1.0"
+
 IMMERSION_POWER               = 1000
 
 MQTT_ADDRESS="localhost"
@@ -24,10 +26,19 @@ energy_import = 0
 power=0
 temperature =0
 immersion_on = 0
+immersion_time = 0
+excess_power=0
+temperature_change=0
 
+def TemperatureChange(t):
+    TemperatureChange.log.append(t)
+    if len(TemperatureChange.log)>10:
+        TemperatureChange.log=TemperatureChange.log[1:]
+    return TemperatureChange.log[-1]-TemperatureChange.log[0]
+TemperatureChange.log=[]
 
 def ExcessPower():
-    global immersion_on
+    global immersion_on, immersion_time,excess_power
     
     excess_power=0
     if immersion_on==0:
@@ -36,12 +47,12 @@ def ExcessPower():
         excess_power = -power + IMMERSION_POWER
 
     print("Excess_power=", excess_power)
-    client.publish("roost/excess_power",   str(excess_power))
 
     if excess_power > IMMERSION_POWER:
         print("Immersion ON")
         immersion_on = 1
         client.publish("cmnd/sonoff/POWER", "ON");        
+        immersion_time+=0.5
 
     else:
         print("Immersion OFF")
@@ -84,19 +95,20 @@ def ExcessPower():
 
 
 def DailyLog():
-    global energy,power,temperature,energy_import, energy_export,samples
-    if samples==120:
+    global energy,power,temperature,energy_import, energy_export,samples,immersion_time,immersion_on
+    if samples==1 : #120:
         print("MIDNIGHT SAVING TOTALS")
         f = open("log.csv", "a")
-        f.write("%s, %d , %.4f , %.4f %.4f\n"  %(time.asctime(), time.time(), energy ,energy_import ,energy_export) )
+        f.write("%s, %d , %.4f , %.4f , %.4f , %.1f , %d\n"  %(time.asctime(), time.time(), energy ,energy_import ,energy_export,temperature,immersion_on) )
         f.close()
     
     # Check for midnight
     if samples>0 and time.strftime("%H", time.gmtime()) =="00" :  
         energy=0
-        energy_import=0
-        energy_export=0
-        samples = 0
+        energy_import   =0
+        energy_export   =0
+        samples         =0
+        immersion_time  =0 
     
     samples+=1
 
@@ -105,7 +117,7 @@ def DailyLog():
 def on_message(client, userdata, message):
     print()
     print("message.topic=",message.topic)
-    global energy,power,temperature,energy_import, energy_export,samples
+    global energy,power,temperature,energy_import, energy_export,samples,immersion_time,excess_power,temperature_change
 
     #print("message received " ,str(message.payload.decode("utf-8")))
     #print("message topic=",message.topic)
@@ -124,12 +136,16 @@ def on_message(client, userdata, message):
            energy_export -= instant_energy
 
         print("energy=%.4f import=%.4f export=%.4f "  %(energy ,energy_import ,energy_export) )
-        client.publish("roost/power",           str(power))
-        client.publish("roost/energy",          str(energy))
-        client.publish("roost/energy_import",   str(energy_import))
-        client.publish("roost/energy_export",   str(energy_export))
-        
         ExcessPower()
+        client.publish("roost/version",            VERSION)
+        client.publish("roost/power",              str(power))
+        client.publish("roost/energy",             str(energy))
+        client.publish("roost/energy_import",      str(energy_import))
+        client.publish("roost/energy_export",      str(energy_export))
+        client.publish("roost/immersion_time",     str(immersion_time/60))
+        client.publish("roost/temperature",        str(temperature))
+        client.publish("roost/excess_power",       str(excess_power))
+        client.publish("roost/temperature_change", str(temperature_change))
         
         DailyLog()
 
@@ -139,6 +155,7 @@ def on_message(client, userdata, message):
        #print(data)
        temperature=data["DS18B20"]["Temperature"]
        print("temperature", temperature)
+       temperature_change=TemperatureChange(temperature)
 
     if message.topic=="cmnd/sonoff/POWER":
 
@@ -185,4 +202,3 @@ while(1):
    #client.publish("cmnd/sonoff/POWER", "OFF");
    #client.publish("roost/energy_import", str(energy_import))
    #energy_import+=0.12
-
