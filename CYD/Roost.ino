742bc7ecd5bba5ebefb11dacc9d8ff6646348ad2
Alistair Campbell <65419433+amcampbell1975@users.noreply.github.com>
2024-10-09 19:31:18 +0100

    Update Roost.ino
    Add temperate and energy graphs
    Quad temp sensor

--- CYD/Roost.ino
+++ CYD/Roost.ino
@@ -8,6 +8,7 @@
 #include "icons.h" // Roost Icons.
 
 // Replace the next variables with your SSID/Password combination
+
 #include "mywifi.h"
 
 // Add your MQTT Broker IP address, example:
@@ -34,14 +35,47 @@ struct tm timeinfo;
 SPIClass mySpi = SPIClass(VSPI);
 XPT2046_Touchscreen ts(XPT2046_CS, XPT2046_IRQ);
 
+#define LCD_BACK_LIGHT_PIN 21
+
+// use first channel of 16 channels (started from zero)
+#define LEDC_CHANNEL_0     0
+// use 12 bit precission for LEDC timer
+#define LEDC_TIMER_12_BIT  12
+// use 5000 Hz as a LEDC base frequency
+#define LEDC_BASE_FREQ     5000
+void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
+  // calculate duty, 4095 from 2 ^ 12 - 1
+  uint32_t duty = (4095 / valueMax) * min(value, valueMax);
+
+  // write duty to LEDC
+  ledcWrite(channel, duty);
+}
+
 /////////////////////////////////////
 
 int   plug_heater=1;
 float power      =0;
 float energy     =0;
 float hot_water_tank_top=0;
+float hot_water_tank_upper=0;
+float hot_water_tank_lower=0;
 float hot_water_tank_bottom=0;
 float hotwater_minutes=0;
+float hotwater_kwh=0;
+float immersion_on=0;
+
+
+float temps_top[320];
+float temps_upp[320];
+float temps_low[320];
+float temps_bot[320];
+
+
+float powers[320];
+float immersion_powers[320];
+
+
+uint32_t water_temp_colour_scale[20];
 
 
 TFT_eSPI tft = TFT_eSPI();
@@ -59,6 +93,8 @@ int limit(int in, int min, int max){
 }
 
 void Energy(float energy){
+
+  Serial.printf("Energy(%f)\n",energy);
   uint32_t colour;
   if(energy>0){
     colour=TFT_RED;
@@ -69,44 +105,234 @@ void Energy(float energy){
   sprite_energy.fillScreen(TFT_BLACK);
   sprite_energy.fillRect(0,0,100,30,TFT_DARKGREY );
   sprite_energy.fillRect(0,0,width,30,colour );
-  sprite_energy.setTextColor(TFT_WHITE, TFT_BLACK );
+  sprite_energy.setTextColor(colour, TFT_BLACK );
   char e[10];  snprintf(e,10,"%.1f",abs(energy));
   sprite_energy.drawString(String(e),101,0, 4);
+  sprite_energy.setTextColor(TFT_WHITE, TFT_BLACK );
   sprite_energy.drawString("kWh",143,15, 1);
   sprite_energy.pushSprite(0, 200, TFT_TRANSPARENT);
 }
 
 
-void HotWater(float top_value, float bottom_value,int hotwater_minutes){
+
+void AddNewHotWaterTop(float t ){
+  for (int i = 0; i <320-1 ; i++){
+    temps_top[i]=temps_top[i+1];
+    }
+  temps_top[319]=t;
+}
+void AddNewHotWaterUpper(float t ){
+  for (int i = 0; i <320-1 ; i++){
+    temps_upp[i]=temps_upp[i+1];
+    }
+  temps_upp[319]=t;
+}
+void AddNewHotWaterLower(float t ){
+  for (int i = 0; i <320-1 ; i++){
+    temps_low[i]=temps_low[i+1];
+    }
+  temps_low[319]=t;
+}
+void AddNewHotWaterBottom(float t ){
+  for (int i = 0; i <320-1 ; i++){
+    temps_bot[i]=temps_bot[i+1];
+    }
+  temps_bot[319]=t;
+}
+
+void AddNewPower(float p ){
+  for (int i = 0; i <320-1 ; i++){
+    powers[i]=powers[i+1];
+    }
+  powers[319]=p;
+}
+void AddNewImmersion(float p ){
+  for (int i = 0; i <320-1 ; i++){
+    immersion_powers[i]=immersion_powers[i+1];
+    }
+  immersion_powers[319]=p;
+}
+
+
+void AddNewHotWater(float t0,float t1,float t2,float t3 ){
+  AddNewHotWaterTop(t0);
+  AddNewHotWaterUpper(t1);
+  AddNewHotWaterLower(t2);
+  AddNewHotWaterBottom(t3);
+}
+
+
+
+void PowersGraph(void){
+  
+
+  const int G_HEIGHT=230;
+  for(int i=0;i<100;i+=10){
+    float h= map(i,0,100,G_HEIGHT,0  );
+    tft.drawFastHLine( 0, h,  320, TFT_DARKGREY);
+  }
+  for(int i=0;i<320;i++){
+     if((320-1-i)%60==0){
+      tft.drawFastVLine( i, 0,  G_HEIGHT, TFT_DARKGREY);
+    }
+  }
+  //Serial.printf("HotGraph()\n");
+  for(int i=0;i<320;i++){
+    //Serial.printf("%d - %f\n",i,temps[i]);
+    float h= map(powers[i],-4000,4000,G_HEIGHT,0  );
+    if (powers[i]>0){
+      tft.drawPixel(i,h , TFT_RED); 
+      tft.drawPixel(i,h+1 , TFT_RED); 
+    }else{
+      tft.drawPixel(i,h , TFT_GREEN); 
+      tft.drawPixel(i,h+1 , TFT_GREEN); 
+    }
+      
+    h= map(immersion_powers[i],-4000,4000,G_HEIGHT,0  );
+    if (immersion_powers[i]>0){
+      tft.drawPixel(i,h , TFT_YELLOW);
+      tft.drawPixel(i,h+1 , TFT_YELLOW);
+    }   
+
+    if((320-1-i)%60==0){
+      tft.drawPixel(i,G_HEIGHT+2 , TFT_WHITE); 
+      tft.drawPixel(i,G_HEIGHT+3 , TFT_WHITE); 
+    }
+  }
+  tft.drawFastHLine( 0, G_HEIGHT+1,  320, TFT_WHITE);
+  
+}
+  
+  
+void HotGraph(void){//float temperatures[10]){
+
+
+
+  const int G_HEIGHT=230;
+  for(int i=0;i<100;i+=10){
+    float h= map(i,0,100,G_HEIGHT,0  );
+    tft.drawFastHLine( 0, h,  320, TFT_DARKGREY);
+  }
+  for(int i=0;i<320;i++){
+     if((320-1-i)%60==0){
+      tft.drawFastVLine( i, 0,  G_HEIGHT, TFT_DARKGREY);
+    }
+  }
+  //Serial.printf("HotGraph()\n");
+  for(int i=0;i<320;i++){
+    //Serial.printf("%d - %f\n",i,temps[i]);
+    float h= map(temps_top[i],0,100,G_HEIGHT,0  );
+    //tft.drawFastVLine(i , 0,      h, TFT_YELLOW);
+    const int index=limit(map(temps_top[i],10,65,19,0),0,19);
+    const uint32_t colour=water_temp_colour_scale[index];
+    tft.drawFastVLine(i , h,  G_HEIGHT-h, colour);
+    tft.drawPixel(i,h , TFT_RED); 
+    if((320-1-i)%60==0){
+      tft.drawPixel(i,G_HEIGHT+2 , TFT_WHITE); 
+      tft.drawPixel(i,G_HEIGHT+3 , TFT_WHITE); 
+    }
+  }
+
+  for(int i=0;i<320;i++){
+    float h;
+    
+    h= map(temps_upp[i],0,100,G_HEIGHT,0  );
+    tft.drawPixel(i,h , TFT_WHITE); 
+    h= map(temps_low[i],0,100,G_HEIGHT,0  );
+    tft.drawPixel(i,h , TFT_WHITE); 
+    h= map(temps_bot[i],0,100,G_HEIGHT,0  );
+    tft.drawPixel(i,h , TFT_WHITE); 
+  }
+  tft.setTextColor(TFT_WHITE, TFT_BLACK );
+  tft.drawString(String(hot_water_tank_top) ,   5,20,  2);
+  tft.drawString(String(hot_water_tank_upper),  5,60,  2);
+  tft.drawString(String(hot_water_tank_lower),  5,100, 2);
+  tft.drawString(String(hot_water_tank_bottom), 5,140, 2);
+
+  tft.drawFastHLine( 0, G_HEIGHT+1,  320, TFT_WHITE);
+
+}
+
+
+
+void HotWater(float top_value,float upper_value, float lower_value, float bottom_value,int hotwater_minutes){
+
+  Serial.printf("HotWater %f %f %f %f %d \n",top_value,upper_value,lower_value, bottom_value,hotwater_minutes);
   int int_top_value   =int(top_value+0.5);
+  int int_upp_value   =int(upper_value+0.5);
+  int int_low_value   =int(lower_value+0.5);
   int int_bot_value   =int(bottom_value+0.5);
 
 //String::
   char top[10];  snprintf(top,10,"%.1f",top_value);
+  char upp[10];  snprintf(upp,10,"%.1f",upper_value);
+  char low[10];  snprintf(low,10,"%.1f",lower_value);
   char bot[10];  snprintf(bot,10,"%.1f",bottom_value);
   
 //  sprite_tank.fillScreen(TFT_DARKGREY);
   sprite_tank.fillScreen(TFT_BLACK);
-  sprite_tank.fillRect(0,0,60,170,TFT_BLUE );
+ 
 
-  int height_top=map(int_top_value, 30,100,0,170); 
-  limit(height_top, 0, 170);
-  int height_bot=map(int_bot_value, 30,100,0,170); 
-  limit(height_bot, 0, 170);
+  //int height_top=map(int_top_value, 30,100,0,170); 
+  //height_top=limit(height_top, 0, 170);
+  //int height_bot=map(int_bot_value, 30,100,0,170); 
+  //height_bot=limit(height_bot, 0, 170);
 
-  sprite_tank.fillRect(0,0,60,height_top,TFT_RED );
+  {
+    const int Y=41*0; 
+    const int index=limit(map(int_top_value,10,65,19,0),0,19);
+    const uint32_t colour=water_temp_colour_scale[index];
+    Serial.printf("index=%d Colour=0x%x\n",index,colour);
+    sprite_tank.fillRect(0,Y,60,41,colour );
+    sprite_tank.setTextColor(TFT_WHITE, colour );
+    sprite_tank.drawString(String(top), 5,  Y+6, 4);
+  }
+  { 
+    const int Y=41*1; 
+    const int index=limit(map(int_upp_value,10,65,19,0),0,19);
+    const uint32_t colour=water_temp_colour_scale[index];
+    sprite_tank.fillRect(0,Y,60,41,colour );
+    //sprite_tank.setTextColor(TFT_WHITE, colour );
+    //sprite_tank.drawString(String(upp), 5,Y+6, 2);
+  }
+
+  { 
+    const int Y=41*2; 
+    const int index=limit(map(int_low_value,10,65,19,0),0,19);
+    const uint32_t colour=water_temp_colour_scale[index];
+    sprite_tank.fillRect(0,Y,60,41,colour );
+    //sprite_tank.setTextColor(TFT_WHITE, colour );
+    //sprite_tank.drawString(String(low), 5,Y+12, 2);
+  }
+  { 
+    const int Y=41*3; 
+    const int index=limit(map(int_bot_value,10,65,19,0),0,19);
+    const uint32_t colour=water_temp_colour_scale[index];
+    sprite_tank.fillRect(0,Y,60,41,colour );
+    sprite_tank.setTextColor(TFT_WHITE, colour );
+    sprite_tank.drawString(String(bot), 5,Y+6, 4);
+  }
+
+
+
+   //sprite_tank.fillRect(0,0,60,170,TFT_BLUE );
   //sprite_tank.fillRect(30,height_top,30,height_top-height_bot,TFT_ORANGE );
 
 
-  sprite_tank.setTextColor(TFT_WHITE, TFT_BLACK );
-  sprite_tank.drawString(String(top), 5,  2, 4);
-  sprite_tank.drawString(String(bot), 5,145, 4);
-  sprite_tank.drawString(String(hotwater_minutes)+" Min", 2,172, 2);
+  //sprite_tank.setTextColor(TFT_WHITE, TFT_BLACK );
+  //sprite_tank.drawString(String(top), 5,  2, 4);
+  //sprite_tank.drawString(String(bot), 5,145, 4);
 
-  sprite_tank.drawRect(0, 0, 60,170,TFT_WHITE );
+  {
+    sprite_tank.setTextColor(TFT_WHITE, TFT_BLACK );
+    sprite_tank.drawString(String(hotwater_minutes)+" Min", 2,166   , 2);
+    sprite_tank.drawString(String(hotwater_kwh    )+" kWh", 2,166+13, 2);
 
+
+    sprite_tank.drawRect(0, 0, 60,41*4,TFT_WHITE );
+  }
   if(plug_heater==1)
-    sprite_tank.pushImage(18, 80,icon_plug_width, icon_plug_height, icon_plug);
+    sprite_tank.pushImage(15, 70,icon_plug_width, icon_plug_height, icon_plug);
 
   sprite_tank.pushSprite(250, 20, TFT_TRANSPARENT);
 }
@@ -152,8 +378,15 @@ void Dial( float value){
 
 void setup() {
   Serial.begin(115200);
+  Serial.printf("Roost version 1.0/n");
+
   tft.init();
   tft.setRotation(1); //This is the display in landscape
+
+  ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_12_BIT);
+  ledcAttachPin(LCD_BACK_LIGHT_PIN, LEDC_CHANNEL_0);
+  
+  ledcAnalogWrite(LEDC_CHANNEL_0, 255); // On full brightness
   
   // sprite_power stuff
   sprite_power.setColorDepth(8);
@@ -169,6 +402,9 @@ void setup() {
 
 
 
+
+
+
   tft.pushImage(10, 200, icon_fire_width, icon_fire_height, icon_fire);
   tft.pushImage(50, 200, icon_sun_width,  icon_sun_height,  icon_sun);
   tft.pushImage(90, 200, icon_grid_width, icon_grid_height, icon_grid);
@@ -180,7 +416,22 @@ void setup() {
   ts.begin(mySpi);
   ts.setRotation(1);
 
+  for(int i=0;i<20;i++){
+    const uint8_t   red  = map(i,0,20,255,  0);
+    const uint8_t   blue = map(i,0,20,  0,255);
+    const uint8_t  green = 0x00;
+    const uint32_t colour= tft.color565(red,  green, blue);
+    water_temp_colour_scale[i]=colour;
+  }
+  for(int i=0;i<320;i++){
+    temps_top[i]=20;
+    temps_upp[i]=20;
+    temps_low[i]=20;
+    temps_bot[i]=20;
+  }
+   
 
+  
   delay(2000);
 
   //while(1) 
@@ -189,18 +440,33 @@ void setup() {
   
     for (int i=0; i< 2000; i+=50){
       Dial(i);
+      AddNewPower(i);
       
       int t=map(i,0,2000,0,99);
-      HotWater(t,t/2,i%120 );
+      HotWater(t, t/2,t/3,t/4,i%120 );
+      AddNewHotWater(t, t/2,t/3,t/4);
       Energy(i/200.0);
+      AddNewImmersion(i%1000);
 
       delay(100);
     }
+    for(int i=0;i<10;i++){
+      delay(300);
+      ledcAnalogWrite(LEDC_CHANNEL_0, 0); // Off
+      delay(300); 
+      ledcAnalogWrite(LEDC_CHANNEL_0, 255); // On full brightness
+    }
     for (int i=0; i> -2000; i-=50){
       Dial(i);
+      AddNewPower(i);
+      AddNewImmersion(i%1000);
+      
       int t=map(i,-2000,0,0,99);
-      HotWater(t,t/3,i%500);
+      HotWater(t,t*0.8, t*0.7, t*0.4,i%500);
+      AddNewHotWater(t,t*0.8, t*0.7, t*0.4);
+
       Energy(i/200.0);
+
 
       delay(100);
     }
@@ -214,6 +480,8 @@ void setup() {
   tft.drawString("Connecting to MQTT " + String(mqtt_server) , 0,20, 2);
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  Serial.println("Call back configured");
+
   tft.drawString("Waiting for update...", 0,40, 2);
 }
 
@@ -241,98 +509,77 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
+  
   String Topic = topic;
+  String Message;
+  for (int i = 0; i < length; i++) {
+    Message += (char)message[i];
+  }
+  Serial.printf("MQTT '%s' '%s'\n",Topic.c_str(),Message.c_str());
 
   if(Topic=="roost/power"){
-    String Message;
-    for (int i = 0; i < length; i++) {
-      Message += (char)message[i];
-    }
     power=Message.toFloat();
- 
-
+    AddNewPower(power);
   }else if(Topic=="roost/plug1_on"){
-    String Message;
-    for (int i = 0; i < length; i++) {
-      //Serial.print((char)message[i]);
-      Message += (char)message[i];
-    }
-    Serial.println(Topic);
-    Serial.println(Message);
     plug_heater=Message.toInt();
-
   }else if(Topic=="roost/hot_water_tank_top"){
-    String Message;
-    for (int i = 0; i < length; i++) {
-      //Serial.print((char)message[i]);
-      Message += (char)message[i];
-    }
-    Serial.println(Topic);
-    Serial.println(Message);
-    
-    {
-        hot_water_tank_top = Message.toFloat();
-        Serial.print("Temp=");
-        Serial.println(hot_water_tank_top);
+    hot_water_tank_top = Message.toFloat();
+    AddNewHotWaterTop(hot_water_tank_top);
+    Serial.print("Temp=");
+    Serial.println(hot_water_tank_top);
 
-    }
+  }else if(Topic=="roost/immersion_on"){
+    immersion_on = Message.toFloat();
+    AddNewImmersion(immersion_on*1000);
+    Serial.print("Immersion on=");
+    Serial.println(immersion_on);
   }else if(Topic=="roost/hot_water_tank_bottom"){
-    String Message;
-    for (int i = 0; i < length; i++) {
-      //Serial.print((char)message[i]);
-      Message += (char)message[i];
-    }
-    Serial.println(Topic);
-    Serial.println(Message);
-    
-    {
-        hot_water_tank_bottom = Message.toFloat();
-        Serial.print("Temp=");
-        Serial.println(hot_water_tank_bottom);
-
-    }
+    hot_water_tank_bottom = Message.toFloat();
+    AddNewHotWaterBottom(hot_water_tank_bottom);
+    Serial.print("Temp=");
+    Serial.println(hot_water_tank_bottom);
   }else if(Topic=="roost/immersion_time"){
-    String Message;
-    for (int i = 0; i < length; i++) {
-      //Serial.print((char)message[i]);
-      Message += (char)message[i];
-    }
-    Serial.println(Topic);
-    Serial.println(Message);
+    hotwater_minutes = Message.toFloat()*60;
+    Serial.print("hotwater_minutes=");
+    Serial.println(hotwater_minutes);
+  }else if(Topic=="roost/hot_water_tank_upper"){
+    hot_water_tank_upper = Message.toFloat();
+    AddNewHotWaterUpper(hot_water_tank_upper);
+    Serial.print("Temp=");
+    Serial.println(hot_water_tank_upper);
+  }else if(Topic=="roost/hot_water_tank_lower"){
+    hot_water_tank_lower = Message.toFloat();
+    AddNewHotWaterLower(hot_water_tank_lower);
+    Serial.print("Temp=");
+    Serial.println(hot_water_tank_lower);
+  }else if(Topic=="roost/immersion_energy_today"){
+    hotwater_kwh = Message.toFloat();
+    Serial.print("hotwater_kwh=");
+    Serial.println(hotwater_kwh);
+
     
-    {
-        hotwater_minutes = Message.toFloat()*60;
-        Serial.print("hotwater_minutes=");
-        Serial.println(hotwater_minutes);
-    }
   }else if(Topic=="roost/energy"){
-    String Message;
-    for (int i = 0; i < length; i++) {
-      //Serial.print((char)message[i]);
-      Message += (char)message[i];
-    }
-    Serial.println(Topic);
-    Serial.println(Message);
-    
-    {
-        energy = Message.toFloat();
-        Serial.print("energy=");
-        Serial.println(energy);
-    }
+    energy = Message.toFloat();
+    Serial.print("energy=");
+    Serial.println(energy);
   }
+
   //only update once on the final mqtt message topic of the set
   if(Topic=="roost/hot_water_tank_bottom"){
     getLocalTime(&timeinfo);
 
-    //Serial.printf("time=%d %d\n",timeinfo.tm_hour,timeinfo.tm_min);
-    tft.fillScreen(TFT_BLACK);
-    if(timeinfo.tm_hour>7 && timeinfo.tm_hour<22){
+    Serial.printf("time=%d %d\n",timeinfo.tm_hour,timeinfo.tm_min);
+    const int DST=0;
+    if(timeinfo.tm_hour+DST>7 && 
+       timeinfo.tm_hour+DST<21)
+    {
+      tft.fillScreen(TFT_BLACK);
+      ledcAnalogWrite(LEDC_CHANNEL_0, 255);
       Dial(power);
-      HotWater(hot_water_tank_top,hot_water_tank_bottom,hotwater_minutes );
+      HotWater(hot_water_tank_top,hot_water_tank_upper,hot_water_tank_lower,   hot_water_tank_bottom,hotwater_minutes );
       Energy(energy);
+    }else{
+      ledcAnalogWrite(LEDC_CHANNEL_0, 0);
     }
   }
 
@@ -357,30 +604,43 @@ void callback(char* topic, byte* message, unsigned int length) {
 }
 
 void reconnect() {
+  int i=0;
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     tft.setTextColor(TFT_WHITE, TFT_BLACK);
     tft.drawString("Attempting MQTT connection." + String(mqtt_server), 0,60, 2);
     // Attempt to connect
-    if (client.connect("ESP8266Client")) {
-      Serial.println("connected to MQTT");
-      tft.drawString("connected to MQTT", 0,80, 2);
+    char mqtt_id[50];
+    sprintf(mqtt_id,"CYD%ld",random(1000));
+    if (client.connect(mqtt_id)) {
+      Serial.println("connected to MQTT "+ String (mqtt_id));
+      Serial.println( mqtt_id);
+      tft.drawString("connected to MQTT "+ String (mqtt_id), 0,80, 2);
       // Subscribe
       //client.subscribe("esp32/output");
       client.subscribe("roost/power");
       client.subscribe("roost/plug1_on");
       client.subscribe("roost/hot_water_tank_top");
+      client.subscribe("roost/hot_water_tank_upper");
+      client.subscribe("roost/hot_water_tank_lower");
       client.subscribe("roost/hot_water_tank_bottom");
       client.subscribe("roost/immersion_time");
-      client.subscribe("roost/energtimeClienty");
-
+      client.subscribe("roost/immersion_energy_today");
+      client.subscribe("roost/immersion_on");
+      client.subscribe("roost/energy");
+      //timeClienty");
       
+      Serial.println( "subscribed to roost/...");
+      
+
+
     } else {
+      ledcAnalogWrite(LEDC_CHANNEL_0, 255);
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      tft.drawString("failed (retry)", 0,100, 2);
+      tft.drawString("failed (retry) "+ String (i++), 0,100, 2);
       // Wait 5 seconds before retrying
       delay(5000);
     }
@@ -404,19 +664,25 @@ void loop() {
 
     Serial.println();
     {
+      ledcAnalogWrite(LEDC_CHANNEL_0, 255); // On full brightness
       tft.fillScreen(TFT_BLACK);
-      Dial(power);
-      HotWater(hot_water_tank_top,hot_water_tank_bottom,hotwater_minutes );
-      Energy(energy);
-    }
+      if (screen_x<50){
+
+        PowersGraph();
+      }else if (screen_x<280){
+        
+        Dial(power);
+        HotWater(hot_water_tank_top,hot_water_tank_upper,hot_water_tank_lower,  hot_water_tank_bottom,hotwater_minutes );
+        Energy(energy);
+      }else{
+          HotGraph();
+      }   
+    } 
   }
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-
-
-
 
 }
  
